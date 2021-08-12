0c52bc7cef2ffe3ee0f4df2a4c311df7d06972fb
such <suchen9209@126.com>
2021-08-12 22:43:48 +0800

    sql update

--- poke.sql
+++ poke.sql
@@ -27,10 +27,6 @@ CREATE TABLE `game` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-/*Data for the table `game` */
-
-insert  into `game`(`id`,`game_name`,`start_time`) values (1,'default','2021-08-08 11:38:58');
-
 /*Table structure for table `game_match` */
 
 DROP TABLE IF EXISTS `game_match`;
@@ -40,7 +36,6 @@ CREATE TABLE `game_match` (
   `game_id` int(11) DEFAULT NULL,
   `small_bind_position` int(11) DEFAULT NULL COMMENT '小盲位',
   `big_bind_position` int(11) DEFAULT NULL COMMENT '大盲位',
-  `button_position` int(11) DEFAULT NULL COMMENT '庄家位',
   `pot_all` int(11) DEFAULT NULL COMMENT '底池',
   `pot1st` int(11) DEFAULT NULL COMMENT '第一轮底池',
   `pot2nd` int(11) DEFAULT NULL COMMENT '第二轮底池',
@@ -49,9 +44,7 @@ CREATE TABLE `game_match` (
   `game_status` enum('INIT','LICENSING','ROUND1','ROUND2','ROUND3','ROUND4','END') DEFAULT NULL,
   `create_time` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `game_match` */
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `game_match_log` */
 
@@ -61,13 +54,11 @@ CREATE TABLE `game_match_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `game_match_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `opreation` varchar(255) DEFAULT NULL,
+  `operation` varchar(255) DEFAULT NULL,
   `point_number` int(11) DEFAULT NULL,
   `add_time` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `game_match_log` */
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
 /*Table structure for table `game_user` */
 
@@ -82,9 +73,7 @@ CREATE TABLE `game_user` (
   `online` tinyint(4) DEFAULT 1 COMMENT '1为在线',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`game_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `game_user` */
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `user` */
 
@@ -99,17 +88,6 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `user` */
-
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (1,'suchot',1000,'2021-08-07 17:31:52','2021-08-07 17:31:52');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (2,'wuke',1000,'2021-08-07 17:31:58','2021-08-07 17:31:58');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (3,'cjx',1000,'2021-08-07 17:32:04','2021-08-07 17:32:04');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (4,'djs',1000,'2021-08-07 17:32:10','2021-08-07 17:32:10');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (5,'muge',1000,'2021-08-07 17:32:17','2021-08-07 17:32:17');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (6,'jt',1000,'2021-08-07 17:32:23','2021-08-07 17:32:23');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (7,'laowang',1000,'2021-08-07 17:32:30','2021-08-07 17:32:30');
-insert  into `user`(`id`,`name`,`point`,`create_time`,`update_time`) values (8,'ashu',1000,'2021-08-07 17:32:33','2021-08-07 17:32:33');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
